081f2e6402b208427e505500f822d3d63c85e108
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-08-23 06:52:01 +0000

    add api reward

--- apis/wallets.js
+++ apis/wallets.js
@@ -1,10 +1,26 @@
 'use strict'
 const express = require('express'),
-  router = express.Router()
+  router = express.Router(),
+  Web3 = require('web3'),
+  { chain } = require('../models/blockchain')
 
 // add new user device
-router.post('/reward', function(req, res) {
-  return res.json({})
+router.post('/reward/:address', async function(req, res) {
+  const receiver = req.params.address
+  const web3 = new Web3(chain)
+  if (!web3.utils.isAddress(receiver)) return res.status(400)
+
+  const amount = 15e18
+  const accounts = await web3.eth.getAccounts()
+  const faucet = {
+    gasPrice: 1,
+    from: accounts[0],
+    to: receiver,
+    value: amount
+  }
+
+  const tx = await web3.eth.sendTransaction(faucet)
+  return res.json(tx)
 })
 
 module.exports = router
--- sockets/index.js
+++ sockets/index.js
@@ -1,13 +1,20 @@
 'use strict'
-
+const Web3 = require('web3')
+const db = require('../models/mongodb')
 const sockets = (io) => {
   io.on('connection', function (socket) {
     console.info('New socket connection', socket.id)
     socket.on('user', function (data) {
       const account = (data.address || '').toLowerCase()
       console.info('New user wallet =', account)
-
-      socket.join(account)
+      if (Web3.utils.isAddress(account)) {
+        db.Wallet.update({
+          walletAddress: account
+        }, { $set: {
+          walletAddress: account
+        } }, { upsert: true })
+        socket.join(account)
+      }
     })
   })
 }
